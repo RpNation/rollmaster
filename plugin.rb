d545b255bc6d69191eb879ed993e60defa5ff042
Alteras1 <42795314+Alteras1@users.noreply.github.com.>
2025-04-16 15:41:47 -0700

    Setup plugin name and common dev tools

--- plugin.rb
+++ plugin.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
+# name: rollmaster
 # about: TODO
 # meta_topic_id: TODO
 # version: 0.0.1
-# authors: Discourse
+# authors: RpNation
 # url: TODO
-# required_version: 2.7.0
+# required_version: 3.0.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :rollmaster_enabled
 
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+module ::Rollmaster
+  PLUGIN_NAME = "discourse-rollmaster"
 end
 
-require_relative "lib/my_plugin_module/engine"
+require_relative "lib/rollmaster/engine"
 
 after_initialize do
   # Code which should run after Rails has finished booting
