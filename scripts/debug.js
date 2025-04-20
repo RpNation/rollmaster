87922ab2ffe4b51530fc3ff2d0573539c55cdcca
Alteras1 <42795314+Alteras1@users.noreply.github.com.>
2025-04-19 17:57:23 -0700

    Add debug file for better dev

---
+++ scripts/debug.js
@@ -0,0 +1,20 @@
+/**
+ * This file is designed to allow devs to debug rpg-dice-roller in a standalone nodejs environment.
+ * Users are advised to start a new nodejs process with the command:
+ *
+ * node ./scripts/debug.js
+ *
+ * This will start a new nodejs REPL with the rpg-dice-roller library loaded.
+ */
+const repl = require("node:repl");
+const { DiceRoller } = require("@dice-roller/rpg-dice-roller");
+
+function roll(...diceRolls) {
+  const roller = new DiceRoller();
+  const rolls = roller.roll(...diceRolls);
+  return rolls;
+}
+
+const r = repl.start({ prompt: "> " });
+r.context.roll = roll;
+r.context.DiceRoller = DiceRoller;
