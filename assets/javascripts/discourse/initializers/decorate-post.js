efa6bcd7909babfc72d16add1c5100d87caac58c
Alteras1 <42795314+Alteras1@users.noreply.github.com.>
2025-10-13 17:11:04 -0700

    Add blockquote formatting for roll results in posts

--- assets/javascripts/discourse/initializers/decorate-post.js
+++ assets/javascripts/discourse/initializers/decorate-post.js
@@ -1,4 +1,5 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
+import { applyValueTransformer } from "discourse/lib/transformer";
 import RollsPostMenuButton from "../components/rolls-post-menu-button";
 
 function decorateCookedElement(el, helper) {
@@ -11,12 +12,36 @@ function decorateCookedElement(el, helper) {
     ".bb-rollmaster[data-notation][data-roll-id]"
   );
   console.log(rollElems, rolls);
+
+  rollElems.forEach((rollElem) => {
+    const rollId = rollElem.getAttribute("data-roll-id");
+    const roll = rolls.find((r) => r.id.toString() === rollId);
+    if (!roll) {
+      return;
+    }
+    // todo: add blockquote around roll result
+    let htmlString = `<blockquote
+   dir="auto"
+   class=".bb-rollmaster-result"
+   data-roll-id="${roll.id}"
+   data-notation="${roll.notation}"
+   data-result="${roll.result}"
+ >${roll.result}</blockquote>`;
+
+    htmlString = applyValueTransformer(
+      "rollmaster-cooked-roll-result",
+      htmlString,
+      { roll, post: helper.getModel(), helper, element: rollElem }
+    );
+    rollElem.outerHTML = htmlString;
+  });
 }
 
 export default {
   name: "rollmaster-decorate-post",
   initialize() {
     withPluginApi((api) => {
+      api.addValueTransformerName("rollmaster-cooked-roll-result");
       api.decorateCookedElement(decorateCookedElement);
 
       api.registerValueTransformer(
